8465e3d23dc54aeebcf1ef443ac638f3a55c75c4
d.smirnov <d.smirnov@cityads.com>
2018-04-23 19:33:58 +0300

    added telegram proxy parameter

--- TelegramNotify.js
+++ TelegramNotify.js
@@ -1,15 +1,21 @@
 const fetch = require('node-fetch');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 class TelegramNotify {
 
-  constructor(telegramToken, telegramAdminChatId) {
-    this.telegramToken = telegramToken;
-    this.telegramAdminChatId = telegramAdminChatId;
+  constructor(config) {
+    this.token = config.token;
+    this.adminChatId = config.adminChatId;
+    this.proxy = config.proxy;
   }
 
   async sendTelegram(message) {
-    let url = 'https://api.telegram.org/bot' + this.telegramToken + '/sendmessage?chat_id=' + this.telegramAdminChatId + '&disable_web_page_preview=1&disable_notification=true&text=' + encodeURIComponent(message);
-    let response = await fetch(url);
+    let url = 'https://api.telegram.org/bot' + this.token + '/sendmessage?chat_id=' + this.adminChatId + '&disable_web_page_preview=1&disable_notification=true&text=' + encodeURIComponent(message);
+    let fetchOptions = {};
+    if (this.proxy) {
+      fetchOptions.agent = new HttpsProxyAgent(this.proxy);
+    }
+    let response = await fetch(url, fetchOptions);
     response = await response.json();
     if (!response) {
       console.error('telegram response is null');
---
+++ readme.md
@@ -0,0 +1,8 @@
+# telegram-notify
+
+#### config
+
+    token  - your telegram bot token
+    adminChatId - telegram chat id for notify
+    proxy - telegram proxy string if needed, e.q. http://login:password@ip:port 
+    
